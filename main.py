53aeec3cab46f3b6949e4b41ac22e426d3470c05
Edwin Zwanenburg <edwinzwanenburg@Macs-MacBook-Pro.local>
2021-04-16 14:16:47 -0500

    added search functionality

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import tkinter
+import os
 from tkinter import messagebox
 import random
+from PIL import ImageTk
+import json
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     pass_input.delete(0, len(pass_input.get()))
@@ -22,30 +33,53 @@ def save_info():
     website = web_input.get()
     email = email_input.get()
     password = pass_input.get()
+    new_data = {website:{
+        "email": email,
+        "password": password
+    }}
 
     if len(website) > 0 and len(email) > 0 and len(password) >= 8:
 
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
 
         if is_ok:
-            with open("./user_info.txt", "a") as file:
-                content = f"{website} | {email} | {password}\n"
-                file.write(content)
+            try:
+                with open("./user_info.json", "r") as file:
+                    data = json.load(file)
+                    data.update(new_data)
+            except:
+                with open("./user_info.json", "w") as file:
+                    json.dump(new_data, file, indent=4)
+            else:
+                with open("./user_info.json", "w") as file:
+                    json.dump(data, file, indent=4)
         web_input.delete(0, len(website))
         pass_input.delete(0, len(password))
     
     else:
         messagebox.showerror(title="Error submitting info.", 
-        message="Fields shouln't be empty and password must be greater than 8 characters.")
+        message="Fields shouldn't be empty and password must be greater than 8 characters.")
 
+def search():
+    search_web = web_input.get()
+
+    try:
+        with open("./user_info.json") as file:
+            data = json.load(file)
+            content = data[search_web]
+            email = content["email"]
+            password = content["password"]
         
+        messagebox.showinfo(title=f"{search_web}", message=f"Email: {email}\nPassword: {password}")
+    except:
+        messagebox.showerror(title="Error on Search", message="Website information not found.")
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 window.minsize(width=430, height=320)
 
-image = tkinter.PhotoImage(file="logo.png")
+image = ImageTk.PhotoImage(file=resource_path("logo.png"))
 
 canvas = tkinter.Canvas(width=200, height=200)
 canvas.create_image(0, 0, image=image, anchor="nw")
@@ -62,8 +96,8 @@ pass_label.grid(column=0, row=3)
 
 #Entries
 
-web_input = tkinter.Entry(width=35)
-web_input.grid(column=1, row=1, columnspan=2)
+web_input = tkinter.Entry(width=21)
+web_input.grid(column=1, row=1)
 web_input.focus()
 email_input = tkinter.Entry(width=35)
 email_input.grid(column=1, row=2, columnspan=2)
@@ -76,6 +110,9 @@ gen_btn = tkinter.Button(text="Generate Password", command=generate_password)
 gen_btn.grid(column=2, row=3, sticky="w")
 add_btn = tkinter.Button(text="Add", width=36, command=save_info)
 add_btn.grid(column=1, row=4, columnspan=2)
+search_btn = tkinter.Button(text="Search", command=search)
+search_btn.grid(column=2, row=1, sticky="e w")
+
 
 
 window.mainloop()
